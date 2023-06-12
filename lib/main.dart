f8639ec4ffff2ed4b7fb2fc6c8b7b7e82665f985
dharm <makwanadharmishtha44@gmail.com>
2023-06-12 18:15:31 +0530

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:firebase_app/screen/views/add_product.dart';
-import 'package:firebase_app/screen/views/notification.dart';
+import 'package:firebase_app/screen/notification_screen/notification.dart';
 import 'package:firebase_app/screen/views/update_product.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
-import 'screen/views/Sign_InScreen.dart';
-import 'screen/views/Sign_UpScreen.dart';
-import 'screen/views/Splesh_Screen.dart';
-import 'screen/views/home_screen.dart';
+import 'screen/views/Login_System/Sign_InScreen.dart';
+import 'screen/views/Login_System/Sign_UpScreen.dart';
+import 'screen/views/Login_System/Splesh_Screen.dart';
+import 'screen/views/All_product.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,13 +25,13 @@ void main() async {
         return GetMaterialApp(
           debugShowCheckedModeBanner: false,
           routes: {
-            '/':(p0) => SplashPage(),
+            '/': (p0) => SplashPage(),
             'SignIn': (p0) => SignInPage(),
-            'SignUp': (p0) =>  SignUpPage(),
+            'SignUp': (p0) => SignUpPage(),
             'Home': (p0) => HomeScreen(),
-            'add': (p0) =>  AddTask(),
-            'note':(p0) => HomePage(),
-            'update':(p0) => UpdateProduct(),
+            'add': (p0) => AddTask(),
+            'note': (p0) => HomePage(),
+            'update': (p0) => UpdateProduct(),
           },
         );
       },
